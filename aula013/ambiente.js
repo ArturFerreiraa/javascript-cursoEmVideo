eb48e6d265c9611656e092c8d8f736d3d2bd2a89
ArturFerreiraa <108987706+ArturFerreiraa@users.noreply.github.com>
2023-08-19 16:08:41 -0300

    Create ambiente.js

---
+++ aula013/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1;
+
+do {
+    console.log(`${c}º Ok`);
+    c++;
+} while (c <= 6);
+
+
+
+while (c <= 10) {
+    console.log(`${c}º Ok`);
+    c++;
+}
+
+
+
+
+/*
+console.log('Ok');
+console.log('Ok');
+console.log('Ok');
+*/
